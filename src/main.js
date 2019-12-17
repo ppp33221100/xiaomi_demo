b579ecc12248755522642bcccba3093e7a9ec134
ppp33221100 <peikunpeng10@163.com>
2019-12-17 14:52:57 +0800

    add

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import  axios from 'axios'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
-
+Vue.prototype.$axios = axios 
+Vue.use(Vant)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+	state:{
+		//存储共享数据
+	},
+	mutations:{
+		//修改数据 同步
+	},
+	actions:{
+		//异步
+	},
+	getters:{
+		
+	}
+})
