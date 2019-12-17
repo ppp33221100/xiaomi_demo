a55ab669965292e8cb1d16216f2815b7b9145491
ppp33221100 <peikunpeng10@163.com>
2019-12-17 18:39:08 +0800

    new

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '../components/Home.vue'
 
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    /*{
+    {
       path: '/',
-      name: '',
-      component: 
-    }*/
+      name: 'Home',
+      component:Home
+    }
   ]
 })
